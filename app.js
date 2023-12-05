6068b96837401e1fd905d18e9649c9191c9bf283
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-04 19:43:28 -0800

    Add index route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
 
 const app = express();
@@ -28,6 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", indexRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
