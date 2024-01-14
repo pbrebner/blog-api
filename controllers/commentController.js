2f6df156274f863fab8bce3abdc54e5c11665106
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-14 15:42:38 -0800

    Fix post update bug on comment delete

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -66,7 +66,7 @@ exports.deleteComment = asyncHandler(async (req, res, next) => {
     if (comment.user._id == req.user._id) {
         const comment = await Comment.findByIdAndDelete(req.params.commentId);
         await Post.findByIdAndUpdate(req.params.postId, {
-            $pull: { comments: comment },
+            $pull: { comments: comment._id },
         });
 
         res.json({
