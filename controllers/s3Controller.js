47088194eb1cdb0100ed0478539dc99f4934aa87
Patrick Brebner <patrick.brebner@gmail.com>
2025-03-04 16:40:39 -0800

    Add function to delete S3 files

--- controllers/s3Controller.js
+++ controllers/s3Controller.js
@@ -5,33 +5,44 @@ const randomBytes = util.promisify(crypto.randomBytes);
 
 const asyncHandler = require("express-async-handler");
 
-const region = "us-west-2";
-const bucketName = "blog-bucket-banana";
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.S3_ACCESS_KEY;
+const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
 
 const s3 = new aws.S3({
-    region,
-    accessKeyId,
+    bucketRegion,
+    accessKey,
     secretAccessKey,
     signatureVersion: "v4",
 });
 
 async function generateUploadURL() {
     const rawBytes = await randomBytes(16);
-    const imageName = rawBytes.toString("hex");
+    const fileName = rawBytes.toString("hex");
 
     const params = {
         Bucket: bucketName,
-        Key: imageName,
-        Expires: 60,
+        Key: fileName,
+        Expires: 90,
     };
 
     const uploadURL = await s3.getSignedUrlPromise("putObject", params);
     return uploadURL;
 }
 
+// Get Upload URL
 exports.getUrl = asyncHandler(async (req, res, next) => {
     const url = await generateUploadURL();
     res.send({ url });
 });
+
+// Delete file from S3
+exports.deleteFileS3 = async (fileName) => {
+    const deleteParams = {
+        Bucket: bucketName,
+        Key: fileName,
+    };
+
+    await s3Client.send(new DeleteObjectCommand(deleteParams));
+};
